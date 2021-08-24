6c402605c54d80f80097041c7bbbf11a0a63c4de
jluissc <jlsc.hco96@gmail.com>
2021-08-23 23:12:41 -0500

    l230821_2

--- admin/modelos/vistasModelo.php
+++ admin/modelos/vistasModelo.php
@@ -5,7 +5,7 @@
 	class vistasModelo{
 
 		protected static function obtener_vistas_M($ruta){
-			$listasVistas = ["client-list","client-new","client-search","client-update","bussines","home","user","products","category","chat-admin","chat-ca","comments","chats-clientes","pedidos"];
+			$listasVistas = ["citas","home"];
 			if(in_array($ruta,$listasVistas)){
 				// si exist el archivo el d eabajo
 				if(is_file("./vistas/contenidos/".$ruta."-view.php")){
@@ -13,7 +13,7 @@
 				} else{
 					$contenido = "404";
 				}
-			} elseif ($ruta =="login" || $ruta == "index") {
+			} elseif ($ruta == "login" || $ruta == "index") {
 				$contenido = "login";
 			}else{
 				$contenido = "404";				
